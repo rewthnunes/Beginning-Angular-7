f2513dbe7075bb21222e812b3f6080f963b4841c
Tinho <rewth@msn.com>
2019-03-03 13:25:16 -0300

    add more one people

--- ng7/src/app/home/home.component.ts
+++ ng7/src/app/home/home.component.ts
@@ -9,6 +9,12 @@ import { DataService } from '../data.service';
 export class HomeComponent implements OnInit {
 
   users: Object;
+  newObject = {
+    avatar: 'https://media.licdn.com/dms/image/C5603AQFsKV6Km5amow/profile-displayphoto-shrink_200_200/0?e=1557360000&v=beta&t=rAAY5Q6BmibutEKSAo3BgYTlZ0YPNehUcK69v2xDvfg',
+    first_name: 'Rewth',
+    id: 4,
+    last_name: 'Nunes'
+  }
 
   constructor(
     private data: DataService
@@ -18,7 +24,8 @@ export class HomeComponent implements OnInit {
     this.data.getUsers().subscribe(
       data => {
         this.users = data;
-        console.log('data -> ', data);
+        this.users.data.push(this.newObject);
+        console.log('data -> ', this.users);
       })
   }
 
